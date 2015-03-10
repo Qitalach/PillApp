ae31fc0f561d05a2bd27240a7bb03001ca08858d
qpeng <qpeng@macalester.edu>
2015-03-10 10:23:56 -0500

    Notification added

---
+++ app/src/main/java/teamqitalach/pillapp/NotificationReceiverActivity.java
@@ -0,0 +1,12 @@
+package teamqitalach.pillapp;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class NotificationReceiverActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_notification_receiver);
+    }
+}
--- app/src/main/java/teamqitalach/pillapp/TodayActivity.java
+++ app/src/main/java/teamqitalach/pillapp/TodayActivity.java
@@ -8,6 +8,10 @@ import android.content.Intent;
 import android.view.View;
 import android.widget.EditText;
 
+import android.app.PendingIntent;
+import android.app.Activity;
+import android.app.Notification;
+import android.app.NotificationManager;
 
 public class TodayActivity extends ActionBarActivity {
 
@@ -71,5 +75,30 @@ public class TodayActivity extends ActionBarActivity {
         // Do something in response to button
         Intent intent = new Intent(this, TomorrowActivity.class);
         startActivity(intent);
+    }
+
+
+
+    public void createNotification(View view) {
+        // Prepare intent which is triggered if the
+        // notification is selected
+        Intent intent = new Intent(this, NotificationReceiverActivity.class);
+        PendingIntent pIntent = PendingIntent.getActivity(this, 0, intent, 0);
+
+        // Build notification
+        // Actions are just fake
+        Notification noti = new Notification.Builder(this)
+                .setContentTitle("Pill Reminder")
+                .setContentText("It's time to take your Vitamin D!").setSmallIcon(R.drawable.abc_ab_share_pack_holo_dark)
+                .setContentIntent(pIntent)
+                .addAction(R.drawable.abc_ab_share_pack_holo_dark, "Snooze", pIntent)
+                .addAction(R.drawable.abc_ab_share_pack_holo_dark, "Taken", pIntent)
+                .addAction(R.drawable.abc_ab_share_pack_holo_dark, "Skip", pIntent).build();
+        NotificationManager notificationManager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+        // hide the notification after its selected
+        noti.flags |= Notification.FLAG_AUTO_CANCEL;
+
+        notificationManager.notify(0, noti);
+
     }
 }
