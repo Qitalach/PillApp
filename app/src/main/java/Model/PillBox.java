9af421635d596a015890e5ff85d7b1e1f4052068
Taylor Keppler <tkeppler@macalester.edu>
2015-03-24 17:36:09 -0500

    add pillBox to model, only comments

---
+++ app/src/main/java/Model/PillBox.java
@@ -0,0 +1,11 @@
+package Model;
+
+/** holds all the Pills
+ * Created by Taylor Rose on 3/24/2015.
+ */
+public class PillBox {
+    // I am a pill box, I hold all your pills --> am I just a data structure?
+    // I have an alarmManager to tell you when your next pillAlarm is?
+    // how do I link to the add activity, etc?
+
+}
