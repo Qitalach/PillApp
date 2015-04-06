229bb8800d813a6402b63094d2fa4650682a5200
Charles Park <charlespk3@gmail.com>
2015-04-06 16:03:18 -0500

    Fixing spaces

--- app/src/main/java/teamqitalach/pillapp/AlertActivity.java
+++ app/src/main/java/teamqitalach/pillapp/AlertActivity.java
@@ -21,18 +21,11 @@ public class AlertActivity extends FragmentActivity {
         super.onCreate(savedInstanceState);
 
 
-
-
-
         /** Creating an Alert Dialog Window */
         AlertAlarm alert = new AlertAlarm();
 
-
-
         /** Opening the Alert Dialog Window. This will be opened when the alarm goes off */
         alert.show(getSupportFragmentManager(), "AlertAlarm");
-
-
 
     }
 
@@ -56,8 +49,8 @@ public class AlertActivity extends FragmentActivity {
         alarmManager = (AlarmManager) getBaseContext().getSystemService(ALARM_SERVICE);
         long currtime =  System.currentTimeMillis();
 
-//time two minutes from now (currtime)
-        long min = currtime+ (long)120000;
+        // time two minutes from now (currtime)
+        long min = currtime + (long)120000;
 
         alarmManager.set(AlarmManager.RTC_WAKEUP, min, operation);
         Toast.makeText(getBaseContext(), "Alarm for " + pillname + " was snoozed", Toast.LENGTH_SHORT).show();
@@ -72,7 +65,7 @@ place holder code allows program to react to pill being taken. currently creats 
 med was taken. eventually this should use the pillname input string to create a history object
  */
 
-    public void doPositiveClick( String pillname){
+    public void doPositiveClick(String pillname){
         Calendar taketime = Calendar.getInstance();
         int hour = taketime.get(Calendar.HOUR_OF_DAY);
         int minute = taketime.get(Calendar.MINUTE);
