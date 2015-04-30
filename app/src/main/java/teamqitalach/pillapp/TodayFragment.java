b803c4de13fec36995ca50499596a3f3ac87b31d
Charles Park <charlespk3@gmail.com>
2015-04-30 18:32:47 -0500

    Hooked up today to database

--- app/src/main/java/teamqitalach/pillapp/TodayFragment.java
+++ app/src/main/java/teamqitalach/pillapp/TodayFragment.java
@@ -36,14 +36,14 @@ public class TodayFragment extends Fragment {
         Calendar calendar = Calendar.getInstance();
         int day = calendar.get(Calendar.DAY_OF_WEEK);
 //        List<Alarm> alarms = pillBox.getAlarms(day);
-        List<Pill> pills = pillBox.getPills(container.getContext());
-        if(pills.size() != 0) {
-            for(Pill pill: pills) {
+//        List<Pill> pills = pillBox.getPills(container.getContext());
+        List<Alarm> alarms = pillBox.getAlarms(container.getContext(), day);
+        if(alarms != null) {
+            for(Alarm alarm: alarms) {
                 TableRow tbrow = new TableRow(container.getContext());
 
                 TextView t1v = new TextView(container.getContext());
-                String name = pill.getPillName();
-                t1v.setText(name);
+                t1v.setText(alarm.getPillName());
                 t1v.setTextColor(Color.WHITE);
                 t1v.setGravity(Gravity.CENTER);
                 t1v.setPadding(30, 30, 30, 30);
@@ -51,31 +51,31 @@ public class TodayFragment extends Fragment {
                 t1v.setTypeface(lightFont);
                 tbrow.addView(t1v);
 
-//                TextView t2v = new TextView(container.getContext());
-//
-//                //changes display to non military time
-//                int nonMilitaryHour = alarm.getHour()%12;
-//                if (nonMilitaryHour == 0){
-//                    nonMilitaryHour=12;
-//                }
-//
-//                //fixes a problem where times were misrepresented "8:4pm" rather than "8:04pm"
-//                String minute;
-//
-//                if (alarm.getMinute() < 10){
-//                    minute = "0" + alarm.getMinute();
-//                } else {
-//                    minute = "" + alarm.getMinute();
-//                }
-//
-//                String time = nonMilitaryHour + ":" + minute + " " + alarm.getAm_pm();
-//                t2v.setText(time);
-//                t2v.setTextColor(Color.WHITE);
-//                t2v.setGravity(Gravity.CENTER);
-//                t2v.setPadding(30, 30, 30, 30);
-//                t2v.setTextSize(25);
-//                t2v.setTypeface(lightFont);
-//                tbrow.addView(t2v);
+                TextView t2v = new TextView(container.getContext());
+
+                //changes display to non military time
+                int nonMilitaryHour = alarm.getHour()%12;
+                if (nonMilitaryHour == 0){
+                    nonMilitaryHour=12;
+                }
+
+                //fixes a problem where times were misrepresented "8:4pm" rather than "8:04pm"
+                String minute;
+
+                if (alarm.getMinute() < 10){
+                    minute = "0" + alarm.getMinute();
+                } else {
+                    minute = "" + alarm.getMinute();
+                }
+
+                String time = nonMilitaryHour + ":" + minute + " " + alarm.getAm_pm();
+                t2v.setText(time);
+                t2v.setTextColor(Color.WHITE);
+                t2v.setGravity(Gravity.CENTER);
+                t2v.setPadding(30, 30, 30, 30);
+                t2v.setTextSize(25);
+                t2v.setTypeface(lightFont);
+                tbrow.addView(t2v);
 //
                 stk.addView(tbrow);
             }
