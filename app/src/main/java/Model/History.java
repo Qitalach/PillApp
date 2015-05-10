801e6f2ee4af3527da0ede4f0a8f2f8d5162244a
lbarstow <lbarstow@macalester.edu>
2015-05-09 20:38:10 -0500

    Description of history class
    see above

--- app/src/main/java/Model/History.java
+++ app/src/main/java/Model/History.java
@@ -2,6 +2,10 @@ package Model;
 
 /**
  * Created by CharlesPK3 on 4/7/15.
+ *
+ * This class represents each history object, an individual incident of a pill being taken.
+ * Each History object contains the name of the pill it is associated with and variables
+ * which represent the time and date at which the medication was taken.
  */
 public class History {
     private int hourTaken;
